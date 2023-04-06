3a7712ea963d9fd02827d2118117d17f193eb2da
Selim Aktay <sellimakty99@gmail.com>
2023-04-06 16:24:58 +0300

    Initial Commit

---
+++ Meta_VideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meta_VideoPlayer
+//
+//  Created by Selim on 6.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
